073e39069262028be365b6267631250e9b644355
nose221834 <22.t.ichinose.nutfes@gmail.com>
2024-03-23 23:33:57 +0900

    sheet情報の追加

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ from src import config
 
 def main():
     files = config.files
+    sheets = config.sheets
 
     # 保存するファイル
     wb_result = openpyxl.Workbook()
+    ws_result = wb_result.active
 
     # 読み込むデータ
     wb_OldUserList = openpyxl.load_workbook(files["OldUserList"])
     wb_newM1List = openpyxl.load_workbook(files["newM1List"])
     wb_newResidualList = openpyxl.load_workbook(files["newResidualList"])
+
+    # シートの読み込み
+    ws_OldUserList = wb_OldUserList[sheets["SheetOldUserList"]]
+    ws_newM1List = wb_newM1List[sheets["SheetNewM1List"]]
+    ws_newResidualList = wb_newResidualList[sheets["SheetNewResidualList"]]
 
     # 保存する
     wb_result.save('files/input_file.xlsx')
--- src/config.py
+++ src/config.py
@@ -7,4 +7,10 @@ OldUserList=os.getenv("OldUserList")
 newM1List = os.getenv("newM1List")
 newResidualList = os.getenv("newResidualList")
 
+SheetOldUserList=os.getenv("SheetOldUserList")
+SheetNewM1List = os.getenv("SheetNewM1List")
+SheetNewResidualList = os.getenv("SheetNewResidualList")
+
+
 files = {"OldUserList":"files/" + OldUserList, "newM1List":"files/"+newM1List, "newResidualList":"files/"+newResidualList}
+sheets = {"SheetOldUserList":SheetOldUserList, "SheetNewM1List":SheetNewM1List, "SheetNewResidualList":SheetNewResidualList}
