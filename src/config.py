5c98a070e14b301b1e3b7b7a69ae5bb93a2dc30e
nose221834 <22.t.ichinose.nutfes@gmail.com>
2024-03-25 16:05:56 +0900

    型ガードを付ける

--- src/config.py
+++ src/config.py
@@ -12,5 +12,10 @@ SheetNewM1List = os.getenv("SheetNewM1List")
 SheetNewResidualList = os.getenv("SheetNewResidualList")
 
 
-files = {"OldUserList":"files/" + OldUserList, "newM1List":"files/"+newM1List, "newResidualList":"files/"+newResidualList}
-sheets = {"SheetOldUserList":SheetOldUserList, "SheetNewM1List":SheetNewM1List, "SheetNewResidualList":SheetNewResidualList}
+def config() :
+    if (OldUserList is None or newM1List is None or newResidualList is None or SheetOldUserList is None or SheetNewM1List is None or SheetNewResidualList is None):
+        return 0
+    else:
+        files = {"OldUserList":"files/" + OldUserList, "newM1List":"files/"+newM1List, "newResidualList":"files/"+newResidualList}
+        sheets = {"SheetOldUserList":SheetOldUserList, "SheetNewM1List":SheetNewM1List, "SheetNewResidualList":SheetNewResidualList}
+        return files,sheets
